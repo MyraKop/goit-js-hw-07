609e7ce580449fca48039b6c6fb6848c9d3abfa3
MyraKop <136658244+MyraKop@users.noreply.github.com>
2023-11-21 16:55:10 -0500

    task-1

---
+++ js/task-01.js
@@ -0,0 +1,13 @@
+const itemList = document.querySelectorAll('.item')
+console.log("Number of categories", itemList.length)
+
+itemList.forEach(element => {
+    const itemNames = element.firstElementChild;
+    console.log('Category:', itemNames.textContent)
+
+    const elementsName = itemNames.nextElementSibling;
+
+    const elementsQuantity = elementsName.children;
+    
+    console.log('Elements:', elementsQuantity.length)
+})
