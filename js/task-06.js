a740531ec03e19345bb4fa8c4e93ab2d92f47023
MyraKop <136658244+MyraKop@users.noreply.github.com>
2023-12-01 16:19:41 -0500

    task -6

--- js/task-06.js
+++ js/task-06.js
@@ -3,3 +3,33 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function createBoxes(amount) {
+    const boxesContainer = document.getElementById('boxes');
+    for (let i = 0; i < amount; i++) {
+      const box = document.createElement('div');
+      box.style.width = `${30 + i * 10}px`;
+      box.style.height = `${30 + i * 10}px`;
+      box.style.backgroundColor = getRandomHexColor();
+      boxesContainer.appendChild(box);
+    }
+  }
+
+  function destroyBoxes() {
+    const boxesContainer = document.getElementById('boxes');
+    boxesContainer.innerHTML = '';
+  }
+
+  document.querySelector('[data-create]').addEventListener('click', function () {
+    const inputAmount = document.querySelector('input');
+    const amount = parseInt(inputAmount.value, 10);
+
+    if (amount >= 1 && amount <= 100) {
+      createBoxes(amount);
+      inputAmount.value = '';
+    } else {
+      alert('Please enter a number between 1 and 100.');
+    }
+  });
+
+  document.querySelector('[data-destroy]').addEventListener('click', destroyBoxes);
