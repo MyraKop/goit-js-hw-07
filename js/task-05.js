6ed2ee4c371cb76e7a9829f37e8df0b20aa2f96d
MyraKop <136658244+MyraKop@users.noreply.github.com>
2023-12-01 16:11:55 -0500

    task-5

--- js/task-05.js
+++ js/task-05.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const bodyElement = document.querySelector("body")
+const spanElement = document.querySelector(".color")
+const buttonElement = document.querySelector(".change-color")
+
+const changeColor = (event) => {
+  bodyElement.style.backgroundColor = getRandomHexColor()
+  spanElement.textContent = bodyElement.style.backgroundColor
+}
+
+buttonElement.addEventListener("click", changeColor)
