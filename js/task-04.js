49ca5926f0c2b7496c6b5f3dddc4ab8b096d2742
MyraKop <136658244+MyraKop@users.noreply.github.com>
2023-12-01 16:08:39 -0500

    task-4

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,21 @@
+const formElement = document.querySelector(".login-form")
 
+const submitHandler = (event) => {
+    event.preventDefault();
+    const {email, password} = event.currentTarget.elements;
+
+
+    if (email.value === "" || password.value === "") {
+     alert("All form fields must be filled in!");
+    } else {
+        const data = {
+            email: email.valid,
+            password: password.value
+        }
+        console.log(data);
+    }
+
+  formElement.reset();
+}
+
+formElement.addEventListener("submit", submitHandler)
