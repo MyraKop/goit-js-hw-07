47a55c29719c6d1a975fc42c92bf003629a6a3f3
MyraKop <136658244+MyraKop@users.noreply.github.com>
2023-12-02 15:34:48 -0500

    updatetask-3

--- js/task-03.js
+++ js/task-03.js
@@ -1,9 +1,7 @@
 const inputName = document.getElementById("name-input") 
 const spanName = document.getElementById("name-output")
 
-inputName.addEventListener("input", (event) => {
-    spanName.textContent = event.currentTarget.value
-    if (inputName.value === "") {
-       spanName.textContent = "Anonymous" 
-    }
-   })
+inputName.addEventListener('input', (event) => {
+    const trimmedValue = event.currentTarget.value.trim();
+    spanName.textContent = trimmedValue || 'Anonymous';
+  });
